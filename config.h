a1710a603dc792f4499fbf60013b749c3f9ffc5b
Oxetene <98855563+Oxetene@users.noreply.github.com>
2022-02-06 20:14:24 +0100

    Indentation fix

--- config.h
+++ config.h
@@ -3,9 +3,9 @@
 #define LENGTH 30
 
 static const Block blocks[] = {
-	/* command			signal */
-	{ "mic",			1 },
-	{ "volume",			2 },
+	/* command		signal */
+	{ "mic",		1 },
+	{ "volume",		2 },
 	{ "battery",		0 },
 	{ "keyboard",		3 },
 	{ "datetime",		0 },
